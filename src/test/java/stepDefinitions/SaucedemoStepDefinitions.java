ce6a9bc7dde0df9e7f6b53882726c08f42d14658
mithrili <altnskihsan@gmail.com>
2024-01-28 16:51:08 +0300

    28.01.2024

--- src/test/java/stepDefinitions/SaucedemoStepDefinitions.java
+++ src/test/java/stepDefinitions/SaucedemoStepDefinitions.java
@@ -2,41 +2,41 @@ package stepDefinitions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import pages.SaucedemoPage;
 
 public class SaucedemoStepDefinitions {
+    SaucedemoPage saucedemoPage = new SaucedemoPage();
+    String firstProduct;
     @Then("Enters {string} into the saucedemo username box.")
-    public void enters_into_the_saucedemo_username_box(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void enters_into_the_saucedemo_username_box(String username) {
+        saucedemoPage.usernameBox.sendKeys(username);
     }
     @Then("Enters {string} into the saucedemo password box.")
-    public void enters_into_the_saucedemo_password_box(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void enters_into_the_saucedemo_password_box(String password) {
+        saucedemoPage.passwordBox.sendKeys(password);
     }
     @Then("Clicks on the login button on saucedemo.")
     public void clicks_on_the_login_button_on_saucedemo() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        saucedemoPage.loginButton.click();
     }
     @Then("Saves the name of the first product and goes to its page.")
     public void saves_the_name_of_the_first_product_and_goes_to_its_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        firstProduct = saucedemoPage.firstProductName.getText();
+        saucedemoPage.firstProductName.click();
     }
-    @When("Clicks on the {string} button on saucedemo.")
-    public void clicks_on_the_button_on_saucedemo(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @When("Clicks on the Add to Cart button on saucedemo.")
+    public void clicksOnTheAddToCartButtonOnSaucedemo() {
+        saucedemoPage.firstProductAddToCartButton.click();
     }
     @Then("Clicks on the shopping cart icon on saucedemo.")
     public void clicks_on_the_shopping_cart_icon_on_saucedemo() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        saucedemoPage.shoppingCart.click();
     }
     @Then("Tests that the selected product is successfully added to the cart.")
     public void tests_that_the_selected_product_is_successfully_added_to_the_cart() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Assert.assertTrue(firstProduct.equals(saucedemoPage.inventoryItem.getText()));
     }
+
+
 }
--- src/test/resources1/features/US1007_SaucedemoShoppingTest.feature
+++ src/test/resources1/features/US1007_SaucedemoShoppingTest.feature
@@ -8,7 +8,7 @@ Feature: US1007 Test for Products Added to the Shopping Cart by the User
     And Waits for 1 seconds.
     Then Clicks on the login button on saucedemo.
     And Saves the name of the first product and goes to its page.
-    When Clicks on the "Add to Cart" button on saucedemo.
+    When Clicks on the Add to Cart button on saucedemo.
     Then Clicks on the shopping cart icon on saucedemo.
     And Waits for 1 seconds.
     And Tests that the selected product is successfully added to the cart.
